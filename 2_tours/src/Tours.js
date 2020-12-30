76bec5c17a459b2fd0e0d3384200a30059e6db47
Andrei Racasan <aracasan@outlook.com>
2020-12-30 12:03:51 +0000

    initial commit - second project: tours

---
+++ 2_tours/src/Tours.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Tour from './Tour';
+
+const Tours = ({tours, removeTour}) => {
+  return <section>
+    <div className="title">
+      <h2>Our Tours</h2>
+      <div className="underline"></div>
+      <div>
+        {tours.map(t => {
+          return <Tour key={t.id} {...t} removeTour={removeTour} />
+        })}
+      </div>
+    </div>
+  </section>;
+};
+
+export default Tours;
